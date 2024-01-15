86a154930d9ca148c7904c471f0ce08ef0c10480
SameedKhan12 <eedsam0@gmil.com>
2024-01-15 15:51:44 +0500

    Second commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "../Footer/Footer";
 import Hero from "../HeroSection/Hero";
 
-import Navbar from "../Navbar/navbar";
+import Navbar from "../Navbar/Navbar";
 import Ad from "./Ad";
 import FAQ from "./FAQ";
 import Features from "./Features";
